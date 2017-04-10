7736bc8c00fd4a333bb9969fb6d02664e0b68a67
prajithkb <prajithkb@gmail.com>
2017-04-10 11:44:16 +0530

    Heaps

--- src/com/hackerrank/solutions/Solution.java
+++ src/com/hackerrank/solutions/Solution.java
@@ -1,7 +1,6 @@
 package com.hackerrank.solutions;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 
 public class Solution {
@@ -45,7 +44,7 @@ public class Solution {
 
     static class Printer {
 
-        static void print(String s) {
+        static void print(Object s) {
             if (canPrint) {
                 System.out.println(s);
             }
@@ -61,15 +60,61 @@ public class Solution {
     }
 
 
+    static class Order {
+        private Long startTime;
+        private Long timeToCook;
+
+        Order(long startTime, long timeToCook) {
+            this.startTime = startTime;
+            this.timeToCook = timeToCook;
+        }
+
+        public Long getStartTime() {
+            return startTime;
+        }
+
+        public Long getTimeToCook() {
+            return timeToCook;
+        }
+
+        public String toString() {
+            return String.format("[%d,%d]", startTime, timeToCook);
+        }
+    }
+
     public static void main(String[] args) {
-//        Scanner in = new Scanner(System.in);
-//        String n = in.next();
+        Scanner in = new Scanner(System.in);
+        long n = in.nextLong();
+        PriorityQueue<Order> orders = new PriorityQueue<Order>(Comparator
+                .comparing(Order::getStartTime));
+        for (int i = 0; i < n; i++) {
+            long l = in.nextLong();
+            long r = in.nextLong();
+            orders.add(new Order(l, r));
+        }
+        long sum = 0;
+        PriorityQueue<Order> best = new PriorityQueue<>(Comparator
+                .comparing(Order::getTimeToCook));
+        long currentTime = 0;
+        while (!orders.isEmpty() || !best.isEmpty()) {
+            while (!orders.isEmpty()
+                    && orders.peek().getStartTime() <= currentTime) {
+                Order o = orders.poll();
+                best.add(o);
+            }
+            if (best.isEmpty() && !orders.isEmpty()) {
+                Order o = orders.poll();
+                best.add(o);
+                currentTime = o.getStartTime();
+            }
+            if( !best.isEmpty()){
+                Order o = best.poll();
+                currentTime += o.getTimeToCook();
+                sum += (currentTime - o.getStartTime());
+            }
 
-        int index = 13;
-        System.out.println(( index & (-index)));
-        System.out.println(index - ( index & (-index)));
-
-
+        }
+        System.out.println(sum / n);
     }
 
 }
