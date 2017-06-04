b20f3c352c8b6e2fe682d064ee50eb3e2eac75b6
prajithkb <prajithkb@gmail.com>
2017-06-04 14:43:25 +0530

    Super happy with solution of a hard tree problem

--- src/com/hackerrank/solutions/Solution.java
+++ src/com/hackerrank/solutions/Solution.java
@@ -1,13 +1,14 @@
+
 package com.hackerrank.solutions;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+import java.io.FileNotFoundException;
 import java.util.*;
 
 
 public class Solution {
 
+    public static final int ROOT = 1;
     private static boolean canPrint;
 
     static {
@@ -37,9 +38,9 @@ public class Solution {
             print("Elapsed Time : " + (System.currentTimeMillis() - startTime));
         }
 
-        private static void print(String text) {
+        private static void print(String time) {
             if (canPrint) {
-                System.out.println(text);
+                System.out.println(time + " ms");
             }
         }
 
@@ -59,217 +60,188 @@ public class Solution {
             }
         }
 
-
     }
 
 
-    static Map<String, Integer> edges = new HashMap<>();
+    static class Tuple {
+        int first;
+        int second;
+        long value;
 
-    static Integer getEdge(int src, int dest) {
-        return edges.get(src + "-" + dest);
-    }
-
-    static void putEdge(int src, int dest, int cost) {
-        if (getEdge(src, dest) == null || cost < getEdge(src, dest)) {
-            edges.put(src + "-" + dest, cost);
-        }
-    }
-
-    static Node node(int index) {
-        if (nodes[index] == null) {
-            nodes[index] = new Node(index);
-        }
-        return nodes[index];
-
-    }
-
-    static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
-
-        public FastReader() {
-            br = new BufferedReader(new
-                    InputStreamReader(System.in));
+        public Tuple() {
         }
 
-        String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
+        public Tuple(int first, int second) {
+            this.first = first;
+            this.second = second;
         }
 
-        int nextInt() {
-            return Integer.parseInt(next());
+        public Tuple(int first, int second, long value) {
+            this.first = first;
+            this.second = second;
+            this.value = value;
         }
 
-        long nextLong() {
-            return Long.parseLong(next());
+        public int getFirst() {
+            return first;
         }
 
-        double nextDouble() {
-            return Double.parseDouble(next());
+        public int getSecond() {
+            return second;
         }
 
-        String nextLine() {
-            String str = "";
-            try {
-                str = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
-        }
-    }
-
-
-    static Node[] nodes;
-
-
-    public static void main(String[] args) {
-
-        FastReader in = new FastReader();
-        int q = 1;
-        for (int j = 0; j < q; j++) {
-            int n = in.nextInt();
-            nodes = new Node[n + 1];
-            int e = in.nextInt();
-            edges = new HashMap<>(2 * e);
-            for (int i = 0; i < e; i++) {
-                Node src = node(in.nextInt());
-                Node dest = node(in.nextInt());
-                int cost = in.nextInt();
-                src.nodes.add(dest);
-                dest.nodes.add(src);
-                putEdge(src.node, dest.node, cost);
-                putEdge(dest.node, src.node, cost);
-            }
-            int s = in.nextInt();
-            int t = in.nextInt();
-            Dijkstra d = new Dijkstra(n, e);
-            d.shortestPath(nodes, s);
-            if (d.distances[t] == Integer.MAX_VALUE) {
-                System.out.print("-1 ");
-            } else {
-                System.out.println(d.distances[t]);
-            }
-        }
-    }
-
-
-    static class Dijkstra {
-        private int distances[];
-        private TreeSet<Edge> priorityQueue;
-        private Node[] nodes;
-        private int[] settled;
-
-        public Dijkstra(int numberOfNodes, int numberOfEdges) {
-            distances = new int[numberOfNodes + 1];
-            Arrays.fill(distances, Integer.MAX_VALUE);
-            settled = new int[numberOfNodes + 1];
-            priorityQueue = new TreeSet<>(Comparator.comparing(Edge::getCost)
-                    .thenComparing(Edge::getSrc)
-                    .thenComparing(Edge::getDest));
+        public void setFirst(int first) {
+            this.first = first;
         }
 
-        public void shortestPath(Node[] nodes, int source) {
-            this.nodes = nodes;
-            priorityQueue.add(new Edge(source, source, 0));
-            int evaluationNode;
-            distances[source] = 0;
-            while (!priorityQueue.isEmpty()) {
-                evaluationNode = getNodeWithMinimumDistanceFromPriorityQueue();
-                settled[evaluationNode] = 1;
-                evaluateNeighbours(evaluationNode);
-            }
+        public void setSecond(int second) {
+            this.second = second;
         }
 
-        private int getNodeWithMinimumDistanceFromPriorityQueue() {
-            Edge e = priorityQueue.pollFirst();
-            return e.dest;
+        public void setValue(long value) {
+            this.value = value;
         }
 
-        private int getCost(int source, int destination) {
-            int edgeDistance = getEdge(source, destination);
-            int newDistance = distances[source] | edgeDistance;
-            return newDistance;
+        public long getValue() {
+            return value;
         }
 
-        private void evaluateNeighbours(int evaluationNode) {
-            int edgeDistance = -1;
-            int newDistance = -1;
-            if (nodes[evaluationNode] == null) {
-                return;
-            }
-            for (int i = 0; i < nodes[evaluationNode].nodes.size(); i++) {
-                Node n = nodes[evaluationNode].nodes.get(i);
-                if (n == null) {
-                    Printer.print("FUCKED ***" + evaluationNode);
-                }
-                if (settled[n.node] == 0) {
-                    newDistance = getCost(evaluationNode, n.node);
-                    if (newDistance < distances[n.node]) {
-                        distances[n.node] = newDistance;
-                        Edge e = new Edge(evaluationNode, n.node, distances[n.node]);
-                        priorityQueue.add(e);
-                    }
-                }
-            }
-        }
-
-
-    }
-
-    static class Node {
-        public int node;
-        public List<Node> nodes = new ArrayList<>();
-
-        public Node() {
-        }
-
-        public Node(int node) {
-            this.node = node;
-        }
-
-        public int getNode() {
-            return node;
+        public long size() {
+            return second - first;
         }
 
         public String toString() {
-            return String.format("(%d)", node);
+            return String.format("(%d,%d) -> %d", first, second, value);
         }
     }
 
-    static class Edge {
-        public int src;
-        public int dest;
-        public Integer cost;
+    static String fileName = "C:\\Users\\kprajith\\Desktop\\x ray.txt";
+    static Scanner in;
 
-        public Edge(int src, int dest, int cost) {
-            this.src = src;
-            this.dest = dest;
-            this.cost = cost;
+    static {
+        try {
+            in = !canPrint ? new Scanner(System.in) : new Scanner(new java.io.File(fileName));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
+    }
 
-        public Integer getCost() {
-            return cost;
+    static int[] nodes;
+
+    static long[] subtreeCount;
+    static long[] redSubtreeCount;
+
+    static long[] subtreeSum;
+
+    static long[] sumFromAllNodes;
+
+    static Map<Integer, List<Tuple>> graph = new HashMap<Integer, List<Tuple>>();
+
+    static void add(int x, int y, long z) {
+        List<Tuple> childrenX = graph.get(x);
+        List<Tuple> childrenY = graph.get(y);
+        if (childrenX == null) {
+            childrenX = new ArrayList<>();
+            graph.put(x, childrenX);
         }
-
-        public int getSrc() {
-            return src;
+        if (childrenY == null) {
+            childrenY = new ArrayList<>();
+            graph.put(y, childrenY);
         }
+        childrenX.add(new Tuple(x, y, z));
+        childrenY.add(new Tuple(y, x, z));
+    }
 
-        public int getDest() {
-            return dest;
+    static boolean isRed(int node) {
+        return nodes[node] == 0;
+    }
+
+    static void visited(int n) {
+        visited = new boolean[n + 1];
+    }
+
+    static int n;
+    static long redNodesCount;
+
+    static boolean[] visited;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Timer.createTimer();
+        n = in.nextInt();
+        subtreeCount = new long[n + 1];
+        redSubtreeCount = new long[n + 1];
+        subtreeSum = new long[n + 1];
+        sumFromAllNodes = new long[n + 1];
+        nodes = new int[n + 1];
+        // populate red/black
+        for (int i = 1; i <= n; i++) {
+            nodes[i] = in.nextInt();
+            redNodesCount += isRed(i) ? 1 : 0;
         }
+        // create the graph
+        for (int i = 0; i < n - 1; i++) {
+            add(in.nextInt(), in.nextInt(), in.nextLong());
+        }
+        visited(n);
+        // Fix node 1 as root, and find all the subtree sums
+        populateSubTreeSum(ROOT);
+        // Populate sumFromAllNodes from all (red) nodes to the current node
+        // Reset the visited the flags;
+        visited(n);
+        populateSumFromAllNodes(ROOT);
+        Printer.print("Graph - : " + graph);
+        Printer.print("subTreeCount : " + Arrays.toString(subtreeCount));
+        Printer.print("subTreeSum : " + Arrays.toString(subtreeSum));
+        Printer.print("sumOfAllNodes : " + Arrays.toString(sumFromAllNodes));
+        long sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (!isRed(i)) {
+                sum += sumFromAllNodes[i];
+            }
+        }
+        System.out.println(sum);
+        Timer.elapsedTime();
 
-        public String toString() {
-            return String.format("(%d ->%d,%d)", src, dest, cost);
+    }
+
+    static void populateSubTreeSum(int node) {
+        visited[node] = true;
+        for (Tuple path : graph.get(node)) {
+            if (!visited[path.getSecond()]) {
+                populateSubTreeSum(path.getSecond());
+                // populate the subTreeCount
+                subtreeCount[node] += subtreeCount[path.getSecond()];
+                // current nodes subTreeSum = sumFromAllNodes of (a child nodes subtree sumFromAllNodes +
+                subtreeSum[node] += subtreeSum[path.getSecond()];
+                // number of red children of the child node * edge child -> node)
+                subtreeSum[node] += (path.getValue() * subtreeCount[path.getSecond()]);
+            }
+        }
+        if (isRed(node)) {
+            // Add the current node to the subtree
+            subtreeCount[node]++;
         }
 
     }
+
+    static void populateSumFromAllNodes(int node) {
+        visited[node] = true;
+        // If the current node is 1 , then it is root so we have it calculated already
+        if (node == ROOT) {
+            sumFromAllNodes[node] = subtreeSum[ROOT];
+        }
+        for (Tuple path : graph.get(node)) {
+            if (!visited[path.getSecond()]) {
+                sumFromAllNodes[path.getSecond()] = sumFromAllNodes[node]
+                        - subtreeCount[path.getSecond()] * path.getValue();
+                long countOfNodesFromParent = redNodesCount - subtreeCount[path.getSecond()];
+                sumFromAllNodes[path.getSecond()] += (countOfNodesFromParent * path.getValue());
+                populateSumFromAllNodes(path.getSecond());
+            }
+        }
+    }
+
+
 }
+
+
