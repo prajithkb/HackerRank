1824c2416760e3ffc171055e7d9eec0e3d038848
Prajith Nair <kprajith@amazon.com>
2018-10-19 22:00:07 +0100

    ConnectedComponents

--- src/com/hackerrank/graphs/ConnectedComponents.java
+++ src/com/hackerrank/graphs/ConnectedComponents.java
@@ -18,6 +18,44 @@ import lombok.ToString;
 @ToString
 public class ConnectedComponents {
 
+
+    @Builder.Default private Map<Integer, Node> nodesMap = new HashMap<>();
+
+    public void connect(int from, int to) {
+        node(from).connectWith(node(to));
+        node(to).connectWith(node(from));
+    }
+    private Node node(int id) {
+        nodesMap.putIfAbsent(id, Node
+                .builder()
+                .value(id)
+                .build());
+        return nodesMap.get(id);
+    }
+    public List<Set<Node>> getConnectedNodes() {
+        Map<Integer, Boolean> visited = new HashMap<>();
+        return nodesMap
+                .entrySet()
+                .stream()
+                .filter(e -> isNotVisited(e.getValue(), visited))
+                .map(e -> createConnectedNodes(e.getValue(), visited))
+                .collect(Collectors.toList());
+
+    }
+
+    private static Set<Node> createConnectedNodes(Node node, Map<Integer, Boolean> visited) {
+        Set<Node> connections = new HashSet<>();
+        node.visit(n -> {
+            visited.put(n.getValue(), true);
+            connections.add(n);
+        }, n -> isNotVisited(n, visited));
+        return connections;
+    }
+
+    private static boolean isNotVisited(Node node, Map<Integer, Boolean> visited){
+        return !visited.getOrDefault(node.getValue(),false);
+    }
+
     @Builder
     @Getter
     public static class Node {
@@ -25,10 +63,9 @@ public class ConnectedComponents {
         private final int value;
 
         @Builder.Default
-        @ToString.Exclude
         private Set<Node> neighbours = new HashSet<>();
 
-        public void connect(Node node) {
+        public void connectWith(Node node) {
             neighbours.add(node);
         }
 
@@ -48,61 +85,5 @@ public class ConnectedComponents {
         }
     }
 
-    @Builder.Default private Map<Integer, Node> nodesMap = new HashMap<>();
-
-    public void connect(int from, int to) {
-        node(from).connect(node(to));
-        node(to).connect(node(from));
-    }
-
-    public Node node(int id) {
-        nodesMap.putIfAbsent(id, Node
-                .builder()
-                .value(id)
-                .build());
-        return nodesMap.get(id);
-    }
-
-    public List<Set<Node>> getConnectedComponentsSizes() {
-        Map<Integer, Boolean> visited = new HashMap<>();
-        return nodesMap
-                .entrySet()
-                .stream()
-                .filter(e -> isNotVisited(e.getValue(), visited))
-                .map(e -> createConnections(e.getValue(), visited))
-                .collect(Collectors.toList());
-
-    }
-
-    public static Set<Node> createConnections(Node node,  Map<Integer, Boolean> visited) {
-        Set<Node> connections = new HashSet<>();
-        node.visit(n -> {
-            visited.put(n.getValue(), true);
-            connections.add(n);
-        }, n -> isNotVisited(n, visited));
-        return connections;
-    }
-
-    static boolean isNotVisited(Node node, Map<Integer, Boolean> visited){
-        return !visited.getOrDefault(node.getValue(),false);
-    }
-
-    static int visit(Node node, Map<Integer, Boolean> visited) {
-        if (visited.getOrDefault(node.getValue(), false)) {
-            return 0;
-        }
-        visited.put(node.getValue(), true);
-        if (node.hasNeighbours()) {
-            return node
-                    .getNeighbours()
-                    .stream()
-                    .filter(n -> !visited.getOrDefault(node.getValue(), false))
-                    .mapToInt(n -> visit(node, visited))
-                    .sum();
-        } else{
-            return 1;
-        }
-
-    }
 
 }
--- src/com/hackerrank/temporary/Solution.java
+++ src/com/hackerrank/temporary/Solution.java
@@ -14,172 +14,11 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import com.hackerrank.graphs.ConnectedComponents;
+
 public class Solution {
 
-    public static class ConnectedComponents {
 
-        ConnectedComponents(Map<Integer, Node> nodesMap) {
-            this.nodesMap = nodesMap;
-        }
-
-        public static ConnectedComponentsBuilder builder() {
-            return new ConnectedComponentsBuilder();
-        }
-
-        public Map<Integer, Node> getNodesMap() {
-            return this.nodesMap;
-        }
-
-        public String toString() {
-            return "ConnectedComponents(nodesMap=" + this.getNodesMap() + ")";
-        }
-
-        public static class Node {
-
-            private final int value;
-
-            private Set<Node> neighbours = new HashSet<>();
-
-            Node(int value, Set<Node> neighbours) {
-                this.value = value;
-                this.neighbours = neighbours;
-            }
-
-            public static NodeBuilder builder() {
-                return new NodeBuilder();
-            }
-
-            public void connect(Node node) {
-                neighbours.add(node);
-            }
-
-            public boolean hasNeighbours() {
-                return !neighbours.isEmpty();
-            }
-
-            public void visit(Consumer<Node> visitor, Function<Node,Boolean> condition){
-                if(condition.apply(this)){
-                    visitor.accept(this);
-                    neighbours.forEach(n -> n.visit(visitor, condition));
-                }
-            }
-
-            public String toString() {
-                return this.getValue() + "";
-            }
-
-            public int getValue() {
-                return this.value;
-            }
-
-            public Set<Node> getNeighbours() {
-                return this.neighbours;
-            }
-
-            public static class NodeBuilder {
-                private int value;
-                private Set<Node> neighbours = new HashSet<>();
-
-                NodeBuilder() {
-                }
-
-                public Node.NodeBuilder value(int value) {
-                    this.value = value;
-                    return this;
-                }
-
-                public Node.NodeBuilder neighbours(Set<Node> neighbours) {
-                    this.neighbours = neighbours;
-                    return this;
-                }
-
-                public Node build() {
-                    return new Node(value, neighbours);
-                }
-
-                public String toString() {
-                    return "ConnectedComponents.Node.NodeBuilder(value=" + this.value + ", neighbours=" + this.neighbours + ")";
-                }
-            }
-        }
-
-        private Map<Integer, Node> nodesMap = new HashMap<>();
-
-        public void connect(int from, int to) {
-            node(from).connect(node(to));
-            node(to).connect(node(from));
-        }
-
-        public Node node(int id) {
-            nodesMap.putIfAbsent(id, Node
-                    .builder()
-                    .value(id)
-                    .build());
-            return nodesMap.get(id);
-        }
-
-        public List<Set<Node>> getConnectedComponentsSizes() {
-            Map<Integer, Boolean> visited = new HashMap<>();
-            return nodesMap
-                    .entrySet()
-                    .stream()
-                    .filter(e -> isNotVisited(e.getValue(), visited))
-                    .map(e -> createConnections(e.getValue(), visited))
-                    .collect(Collectors.toList());
-
-        }
-
-        public static Set<Node> createConnections(Node node,  Map<Integer, Boolean> visited) {
-            Set<Node> connections = new HashSet<>();
-            node.visit(n -> {
-                visited.put(n.getValue(), true);
-                connections.add(n);
-            }, n -> isNotVisited(n, visited));
-            return connections;
-        }
-
-        static boolean isNotVisited(Node node, Map<Integer, Boolean> visited){
-            return !visited.getOrDefault(node.getValue(),false);
-        }
-
-        static int visit(Node node, Map<Integer, Boolean> visited) {
-            if (visited.getOrDefault(node.getValue(), false)) {
-                return 0;
-            }
-            visited.put(node.getValue(), true);
-            if (node.hasNeighbours()) {
-                return node
-                        .getNeighbours()
-                        .stream()
-                        .filter(n -> !visited.getOrDefault(node.getValue(), false))
-                        .mapToInt(n -> visit(node, visited))
-                        .sum();
-            } else{
-                return 1;
-            }
-
-        }
-
-        public static class ConnectedComponentsBuilder {
-            private Map<Integer, Node> nodesMap = new HashMap<>();
-
-            ConnectedComponentsBuilder() {
-            }
-
-            public ConnectedComponents.ConnectedComponentsBuilder nodesMap(Map<Integer, Node> nodesMap) {
-                this.nodesMap = nodesMap;
-                return this;
-            }
-
-            public ConnectedComponents build() {
-                return new ConnectedComponents(nodesMap);
-            }
-
-            public String toString() {
-                return "ConnectedComponents.ConnectedComponentsBuilder(nodesMap=" + this.nodesMap + ")";
-            }
-        }
-    }
 
     static int[] componentsInGraph(int[][] gb) {
         ConnectedComponents connectedComponents = ConnectedComponents
@@ -188,7 +27,7 @@ public class Solution {
         for (int i = 0; i < gb.length; i++) {
             connectedComponents.connect(gb[i][0], gb[i][1]);
         }
-        final List<Set<ConnectedComponents.Node>> connectedNodes = connectedComponents.getConnectedComponentsSizes();
+        final List<Set<ConnectedComponents.Node>> connectedNodes = connectedComponents.getConnectedNodes();
         final List<Integer> sizes = connectedNodes
                 .stream()
                 .mapToInt(cc -> cc.size())
